325fde813a0355b6125a170dcc1f211113e06b03
JustToTryModels <zlibbrary7@gmail.com>
2025-12-13 14:00:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ st.markdown("""
     
     /* INPUT CARDS - Light Cream Background */
     .feature-card {
-        background-color: #FBF1F2;
+        background-color: #F4F6F8;
         border-radius: 12px;
         padding: 1.5rem;
         box-shadow: 0 4px 12px rgba(0,0,0,0.05);
