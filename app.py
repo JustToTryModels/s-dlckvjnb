9bcfac9cb6ad49c80153b40cfa2a1bf9dde92772
JustToTryModels <zlibbrary7@gmail.com>
2025-12-20 20:26:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ st.markdown(
 /* Change font to Tiempos for the entire app with global font size */
 * { 
     font-family: 'Tiempos', 'Tiempos Text', Georgia, serif !important;
-    font-size: 20px !important;
+    font-size: 14px !important;
 }
 
 h1 {
