d59fd79bff8a784aa66c8c36312f2ae162f20a73
JustToTryModels <zlibbrary7@gmail.com>
2025-12-25 16:47:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def load_symspell_model():
 
 @st.cache_resource
 def load_gliner_model():
-    model = GLiNER.from_pretrained("gliner-community/gliner_medium-v2.5")
+    model = GLiNER.from_pretrained("gliner-community/gliner_small-v2.5")
     return model
 
 @st.cache_resource(show_spinner=False)
